ff268f4cabff651a04e0f66260e058cd2b2ae9ea
httpharshjindal <httpharshjindal@gmail.com>
2025-02-21 18:05:17 +0530

    config

--- frontend/app/bookmark/page.tsx
+++ frontend/app/bookmark/page.tsx
@@ -5,7 +5,7 @@ import BookmarkedArticles from '@/components/BookmarkedArticles'
 export default function Bookmarks() {
   return (
     <div>
-      <h1 className='text-2xl font-bold text-center absolute top-10 left-1/2 -translate-x-1/2 -translate-y-1/2 '>Favorite Articles</h1>
+      <h1 className='text-sm md:text-2xl font-bold text-center absolute top-30 sm:top-10 left-1/2 -translate-x-1/2 -translate-y-1/2 '>Favorite Articles</h1>
       <BookmarkedArticles />
     </div>
   )
